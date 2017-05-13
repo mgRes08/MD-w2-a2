a9bc4af573f36653dcf5fb985eebe0cea76f13be
mgRes08 <mgresurreccion2016@gmail.com>
2017-05-13 15:57:30 +0800

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,5 @@
+
+document.querySelector("#appdemo2").innerHTML = "<button type='button' class='btn btn-primary' id='showname'>Click Me</button>";
+$('#showname').click(function(){
+	alert("I am GRACE");
+});
